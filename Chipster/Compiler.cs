fc1e7ebb1a680b9a35ff190672e8959497a9fd32
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-02-02 16:51:22 +0000

    Started implementing Compiler, which will compile and decompile ROMs to text files

---
+++ Chipster/Compiler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chipster
+{
+    static class Compiler
+    {
+        public static void Compile(string codePath, string romPath)
+        {
+
+        }
+
+        public static void Decompile(string romPath, string codePath)
+        {
+            FileStream reader = new FileStream(romPath, FileMode.Open);
+            StreamWriter writer = new StreamWriter(codePath);
+
+            //ROMs are copied into memory starting at location 512 (0x200)
+            int currentLine = 512;
+
+            //A buffer used to store the current instruction, which is always 2 bytes long
+            byte[] currentInstructionBuffer = new byte[2];
+
+            //A string representation of the current instruction
+            string currentInstruction;
+
+            while (reader.Read(currentInstructionBuffer, currentLine - 512, 2) != 0)
+            {
+                
+            }
+        }
+    }
+}
