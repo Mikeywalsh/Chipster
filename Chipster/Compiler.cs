b7de29c8e1b1941711a3c06fb7f00ea086103818
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-02-05 04:15:22 +0000

    Changed the way unknown opcodes are represented in the decompiler

--- Chipster/Compiler.cs
+++ Chipster/Compiler.cs
@@ -34,9 +34,9 @@ namespace Chipster
                     ushort opcode = (ushort)(byte1 << 8 | byte2);
                     string instructionString = HexHelper.ByteToHex(currentInstruction);
                     bool unknownOpcode = false;
+                    string currentLineText = "";
 
-                    writer.Write("0x" + HexHelper.UshortToHex(currentLine).Substring(1) + ": " + instructionString + "   -");
-                    currentLine++;
+                    currentLineText += "0x" + HexHelper.UshortToHex(currentLine).Substring(1) + ": " + instructionString + "   -";
 
                     switch (opcode & 0xF000)
                     {
@@ -46,11 +46,11 @@ namespace Chipster
                             {
                                 //00E0 - Clear the screen
                                 case 0xE0:
-                                    writer.Write("Clear the screen");
+                                    currentLineText += "Clear the screen";
                                     break;
                                 //OOEE - Returns from a subroutine
                                 case 0xEE:
-                                    writer.Write("Return from current subroutine");
+                                    currentLineText += "Return from current subroutine";
                                     break;
                                 default:
                                     unknownOpcode = true;
@@ -59,31 +59,31 @@ namespace Chipster
                             break;
                         //1NNN - Jump to address NNN
                         case 0x1000:
-                            writer.Write("Jump to address 0x" + instructionString.Substring(1));
+                            currentLineText += "Jump to address 0x" + instructionString.Substring(1);
                             break;
                         ////2NNN - Calls subroutine at NNN
                         case 0x2000:
-                            writer.Write("Call subroutine at 0x" + instructionString.Substring(1));
+                            currentLineText += "Call subroutine at 0x" + instructionString.Substring(1);
                             break;
                         //3XNN - Skip the next instruction if RX equals NN
                         case 0x3000:
-                            writer.Write("Skip the next instruction if the value at R" + instructionString[1] + " equals 0x" + instructionString.Substring(2));
+                            currentLineText += "Skip the next instruction if the value at R" + instructionString[1] + " equals 0x" + instructionString.Substring(2);
                             break;
                         //4XNN - Skip the next instruction if RX doesn't equal NN
                         case 0x4000:
-                            writer.Write("Skip the next instruction if the value at R" + instructionString[1] + " does not equal " + instructionString.Substring(2));
+                            currentLineText += "Skip the next instruction if the value at R" + instructionString[1] + " does not equal " + instructionString.Substring(2);
                             break;
                         //5XY0 - Skips the next instruction if RX equals RY
                         case 0x5000:
-                            writer.Write("Skip the next instruction if the value at R" + instructionString[1] + " equals the value at R" + instructionString[2]);
+                            currentLineText += "Skip the next instruction if the value at R" + instructionString[1] + " equals the value at R" + instructionString[2];
                             break;
                         //6XNN - Sets register X to the value of NN
                         case 0x6000:
-                            writer.Write("Set the value of R" + instructionString[1] + " to 0x" + instructionString.Substring(2));
+                            currentLineText += "Set the value of R" + instructionString[1] + " to 0x" + instructionString.Substring(2);
                             break;
                         //7XNN - Adds NN to the value stored at RX
                         case 0x7000:
-                            writer.Write("Adds the value of 0x" + instructionString.Substring(2) + " to R" + instructionString[1]);
+                            currentLineText += "Adds the value of 0x" + instructionString.Substring(2) + " to R" + instructionString[1];
                             break;
                         //Series of opcodes beginning with 8 that perform various register arithmatic
                         case 0x8000:
@@ -91,39 +91,39 @@ namespace Chipster
                             {
                                 //8XY0 - Set RX to the value of RY
                                 case 0x00:
-                                    writer.Write("Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[2]);
+                                    currentLineText += "Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[2];
                                     break;
                                 //8XY1 - Sets RX to RX or RY
                                 case 0x01:
-                                    writer.Write("Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[1] + " OR R" + instructionString[2]);
+                                    currentLineText += "Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[1] + " OR R" + instructionString[2];
                                     break;
                                 //8XY2 - Sets RX to RX and RY
                                 case 0x02:
-                                    writer.Write("Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[1] + " AND R" + instructionString[2]);
+                                    currentLineText += "Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[1] + " AND R" + instructionString[2];
                                     break;
                                 //8XY3 - Sets RX to RX xor RY
                                 case 0x03:
-                                    writer.Write("Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[1] + " XOR R" + instructionString[2]);
+                                    currentLineText += "Sets the value of R" + instructionString[1] + "to the value of R" + instructionString[1] + " XOR R" + instructionString[2];
                                     break;
                                 //8XY4 - Adds RY to RX, RF is set to 1 if carry, 0 if not.
                                 case 0x04:
-                                    writer.Write("Adds the value of R" + instructionString[2] + "to R" + instructionString[1] + " RF is set to 1 if there is a carry, 0 if not");
+                                    currentLineText += "Adds the value of R" + instructionString[2] + "to R" + instructionString[1] + " RF is set to 1 if there is a carry, 0 if not";
                                     break;
                                 //8XY5 - RX = RX - RY, RF is set to NOT borrow
                                 case 0x05:
-                                    writer.Write("Subtracts the value of R" + instructionString[2] + " from R" + instructionString[1] + " RF is set to 0 if there is a borrow, 1 if not");
+                                    currentLineText += "Subtracts the value of R" + instructionString[2] + " from R" + instructionString[1] + " RF is set to 0 if there is a borrow, 1 if not";
                                     break;
                                 //8XY6 - Shift RX right by 1
                                 case 0x06:
-                                    writer.Write("Shift R" + instructionString[1] + "right by one");
+                                    currentLineText += "Shift R" + instructionString[1] + "right by one";
                                     break;
                                 //8XY7 - RX = RY - RX, RF is set to NOT borrow - MAY NOT WORK
                                 case 0x07:
-                                    writer.Write("Sets R" + instructionString[1] + "to the value of R" + instructionString[2] + " minus R" + instructionString[1] + " RF is set to 0 if there is a borrow, 1 if not");
+                                    currentLineText += "Sets R" + instructionString[1] + "to the value of R" + instructionString[2] + " minus R" + instructionString[1] + " RF is set to 0 if there is a borrow, 1 if not";
                                     break;
                                 //8XYE - Shift RX left by 1
                                 case 0x0E:
-                                    writer.Write("Shift R" + instructionString[1] + "left by one");
+                                    currentLineText += "Shift R" + instructionString[1] + "left by one";
                                     break;
                                 default:
                                     unknownOpcode = true;
@@ -132,19 +132,19 @@ namespace Chipster
                             break;
                         //9XY0 - Skips the next instruction if RX doesn't equal RY
                         case 0x9000:
-                            writer.Write("Skip the next instruction if R" + instructionString[1] + " does not equal R" + instructionString[2]);
+                            currentLineText += "Skip the next instruction if R" + instructionString[1] + " does not equal R" + instructionString[2];
                             break;
                         //ANNN - Set the Index register to the value of NNN
                         case 0xA000:
-                            writer.Write("Set the Index register to the value of 0x" + instructionString.Substring(1));
+                            currentLineText += "Set the Index register to the value of 0x" + instructionString.Substring(1);
                             break;
                         //CXNN - Set RX to the result of an and operation on a random number and NN
                         case 0xC000:
-                            writer.Write("Set R" + instructionString[1] + " to the result of an AND operation on a random number and 0x" + instructionString.Substring(2));
+                            currentLineText += "Set R" + instructionString[1] + " to the result of an AND operation on a random number and 0x" + instructionString.Substring(2);
                             break;
                         //DXYN - Draw sprite starting in Index at location (RX, RY) with N height
                         case 0xD000:
-                            writer.Write("Draw sprite starting in index at location (R" + instructionString[1] + ", R" + instructionString[2] + ") with " + instructionString[3] + " height");
+                            currentLineText += "Draw sprite starting in index at location (R" + instructionString[1] + ", R" + instructionString[2] + ") with " + instructionString[3] + " height";
                             break;
                         //opcodes to do with key presses
                         case 0xE000:
@@ -152,11 +152,11 @@ namespace Chipster
                             {
                                 //EX9E - Skip next instruction if key at x is pressed
                                 case 0x9E:
-                                    writer.Write("Skip next instruction if key at " + instructionString[1] + " is pressed");
+                                    currentLineText += "Skip next instruction if key at " + instructionString[1] + " is pressed";
                                     break;
                                 //EXA1 - Skip next instruction if key at x is NOT pressed
                                 case 0xA1:
-                                    writer.Write("Skip next instruction if key at " + instructionString[1] + " is notpressed");
+                                    currentLineText += "Skip next instruction if key at " + instructionString[1] + " is notpressed";
                                     break;
                                 default:
                                     unknownOpcode = true;
@@ -169,39 +169,39 @@ namespace Chipster
                             {
                                 //FX07 - Set RX to the value of the delay timer
                                 case 0x07:
-                                    writer.Write("Set R" + instructionString[1] + " to the value of the delay timer");
+                                    currentLineText += "Set R" + instructionString[1] + " to the value of the delay timer";
                                     break;
                                 //FX0A - Await key press then store result in RX
                                 case 0x0A:
-                                    writer.Write("Await key press, then store the result in R" + instructionString[1]);
+                                    currentLineText += "Await key press, then store the result in R" + instructionString[1];
                                     break;
                                 //FX15 - Set the value of the delay timer to be equal to RX
                                 case 0x15:
-                                    writer.Write("Set the value of the delay timer to be equal to R" + instructionString[1]);
+                                    currentLineText += "Set the value of the delay timer to be equal to R" + instructionString[1];
                                     break;
                                 //FX15 - Set the value of the sound timer to be equal to RX
                                 case 0x18:
-                                    writer.Write("Set the value of the sound timer to be equal to R" + instructionString[1]);
+                                    currentLineText += "Set the value of the sound timer to be equal to R" + instructionString[1];
                                     break;
                                 //FX1E - Adds RX to I and sets RF to 1 if range overflow, 0 if not
                                 case 0x1E:
-                                    writer.Write("Add R" + instructionString[1] + " to Index and sets RF to 1 if there is an overflow, 0 if not");
+                                    currentLineText += "Add R" + instructionString[1] + " to Index and sets RF to 1 if there is an overflow, 0 if not";
                                     break;
                                 //FX29 - Sets I to the location of the sprite for the character in RX
                                 case 0x29:
-                                    writer.Write("Set Index to the location of the sprite for the character in R" + instructionString[1]);
+                                    currentLineText += "Set Index to the location of the sprite for the character in R" + instructionString[1];
                                     break;
                                 //FX33 - Stores the BCD representation of RX
                                 case 0x33:
-                                    writer.Write("Store the BCD representation of R" + instructionString[1] + " in memory, starting at the address in Index");
+                                    currentLineText += "Store the BCD representation of R" + instructionString[1] + " in memory, starting at the address in Index";
                                     break;
                                 //FX55 - Stores R0 to RX(inclusive) in memory starting at the address in Index
                                 case 0x55:
-                                    writer.Write("Store R0 to R" + instructionString[1] + " to memory values starting at the address in Index");
+                                    currentLineText += "Store R0 to R" + instructionString[1] + " to memory values starting at the address in Index";
                                     break;
                                 //FX65 - Fills R0 to RX(inclusive) from memory values starting at the address in Index
                                 case 0x65:
-                                    writer.Write("Fill R0 to R" + instructionString[1] + " from memory values starting at the address in Index");
+                                    currentLineText += "Fill R0 to R" + instructionString[1] + " from memory values starting at the address in Index";
                                     break;
                                 default:
                                     unknownOpcode = true;
@@ -215,12 +215,10 @@ namespace Chipster
                     }
 
                     if (unknownOpcode)
-                    {
-                        writer.Write("Unknown Opcode");
                         break;
-                    }
 
-                    writer.WriteLine();
+                    writer.WriteLine(currentLineText);
+                    currentLine++;
                 }
 
                 MessageBox.Show("Decompiled " + romPath + "\nStored results in " + codePath);
