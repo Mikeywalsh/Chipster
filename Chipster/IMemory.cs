191d57f021ded69069871eb071529b5aa8c49626
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2018-02-12 13:59:20 +0000

    Removed more unnessessary using statements

--- Chipster/IMemory.cs
+++ Chipster/IMemory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chipster
+﻿namespace Chipster
 {
     /// <summary>
     /// Interface used for classes wishing to implement memory
